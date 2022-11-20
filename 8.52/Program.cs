a071053c59502262e8f33e978ffd60c123d905b5
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:18:29 +0300

    Add files via upload

---
+++ 8.52/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8._52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            for (int b = 0; b < 10; b++)
+            {
+                for (int c = 0; c < 20; c++)
+                {
+                    a = 100 - (b + c);
+                    if (20 * b + 10 * c + a == 200)
+                    {
+                        Console.WriteLine("Телят " + a);
+                        Console.WriteLine("Быков " + b);
+                        Console.WriteLine("Коров " + c); 
+                    }
+                }
+            }
+        }
+    }
+}
